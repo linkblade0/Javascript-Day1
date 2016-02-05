f72fef5fe09f6db6ffc8a803dafb9e5e68a813ee
Joseph Lago <josephlago90@gmail.com>
2016-02-05 15:40:55 -0700

    first assignment done

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -6,6 +6,13 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
+function isTyler(name){
+  if (name === 'Tyler') {
+    return true;
+  } else {
+      return false;
+    };
+}
 
 //Next problem
 
@@ -15,6 +22,10 @@ var name = 'Tyler';
 
 
   //Code Here
+function getName() {
+  var name = prompt('What is your name?');
+  return name;
+}
 
 
 //Next Problem
@@ -25,8 +36,10 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
-
+function welcome(){
+  var greeting = ("Welcome " + getName());
+  return greeting;
+}
 //Next problem
 
 
@@ -35,7 +48,8 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
-
+//parameters are a placeholder for a method
+//arguments are expressions used when calling the method
 
 //Next problem
 
@@ -45,8 +59,12 @@ var name = 'Tyler';
 
 
   //Answer Here
-
-
+// 0
+// ''
+// false
+// null
+// NaN
+// undefined
 
 //Next Problem
 
@@ -55,7 +73,11 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+function myName(){
+  var name = "Joey";
+  return name;
+}
+myName();
 
 
 //Now save the function definition of myName into a new variable called newMyName
@@ -63,7 +85,7 @@ var name = 'Tyler';
   //Code Here
 
 //Now alert the result of invoking newMyName
-
+alert(myName());
 
 
 //Next problem
@@ -73,9 +95,15 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn(){
+  return function () {
+    var name = "Joey";
+    return name;
+  }
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn();
 //Now invoke innerFn.
+innerFn();
